15098f0eca3c14b428afe28abc01d6ecae20f3cc
Komlavi-Djossou <djokwa13@gmail.com>
2022-03-12 15:12:59 +0000

    w10

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -13,9 +13,38 @@ let contentOptions = [
   {link: 'week6/index.html', text: 'Week 6'},
   {link: 'week7/index.html', text: 'Week 7'},
   {link: 'week8/index.html', text: 'Week 8'},
-  {link: 'week9/index.html', text: 'Week 9'}
+  {link: 'week9/index.html', text: 'Week 9'},
+  {link: 'week10/index.html', text: 'Week 10'}
 ];
 
 for ( {link, text} of contentOptions){
   content.innerHTML += `<li> <a id="${text}" href="${link}">${text}<a/> </li>`;
 }
+
+// scrolled events
+window.addEventListener('scroll', () =>{
+ const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+ const scrolled = window.scrollY;
+
+ if (Math.ceil(scrolled) === scrollable) {
+   alert('You\'ve reached the end');
+ }
+});
+
+///menu
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
---
+++ portfolio/week10/earthquake/js/utilities.js
@@ -0,0 +1,21 @@
+export function getJSON(url = "'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02'") {
+    return fetch(url)
+    .then(function(response){
+        if (!response.ok) {
+            throw Error(response.statusText);
+        } else {
+            // console.log(response.json());
+            return response.json();
+        }
+    }) 
+     .catch(function(error) {
+        console.log(error);
+    });
+
+}
+
+export const getLocation = function(options) {
+    return new Promise(function(resolve, reject){
+        navigator.geolocation.getCurrentPosition(resolve,reject,options)
+    });
+};
